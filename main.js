4b7dc988742c4c75242d73689b3176c56680ab6e
dom davies <domdaviesit@gmail.com>
2018-09-02 13:45:17 +0100

    capture user input

--- main.js
+++ main.js
@@ -8,3 +8,17 @@ var button = document.querySelector('button');
 button.addEventListener('click', function() {
     // alert("you have clicked the button"); Test
 })
+
+
+// Capture user input and stop whole page refreshing on submit
+
+var formField = document.querySelector('form');
+
+var input = document.getElementById('user-todo');
+
+formField.addEventListener('submit', function(e) {
+    // keep page from refreshing
+    e.preventDefault();
+    // test
+    alert(input.value)
+})
