1e3f384d32458cd827918d76f7e94cef91f7450a
dom davies <domdaviesit@gmail.com>
2018-09-02 17:10:32 +0100

    added local storage

--- main.js
+++ main.js
@@ -12,11 +12,33 @@ var input = document.getElementById('user-todo');
 
 
 // Action, then call a function (passing in the event (e))
-form.addEventListener('submit', function(e) {
+// form.addEventListener('submit', function(e) {
     // keep page from refreshing
-    e.preventDefault();
+    // e.preventDefault();
     // test
     // alert(input.value)
+
+//     todoMaker(input.value);
+//     input.value = "";
+// });
+
+
+
+// Declare variable 'todosArray' to hold our todos.  set it to empty array
+// var todosArray = [];
+
+var todosArray = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) :[];
+
+localStorage.setItem('todos', JSON.stringify(todosArray));
+
+var storage = JSON.parse(localStorage.getItem('todos'));
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    todosArray.push(input.value);
+
+    localStorage.setItem('todos', JSON.stringify(todosArray));
 
     todoMaker(input.value);
     input.value = "";
@@ -28,8 +50,13 @@ var todoMaker = function(text){
     todoList.appendChild(todo);
 }
 
+for (var i = 0; i < storage.length;i++) {
+    todoMaker(storage[i]);
+}
+
 button.addEventListener('click', function() {
     // alert("you have clicked the button"); Test
+    localStorage.clear();
     while (todoList.firstChild) {
         todoList.removeChild(todoList.firstChild);
     }
