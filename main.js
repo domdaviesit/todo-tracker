946a99d7e0a670e5c9dfd38df03ad0e79e68db76
dom davies <domdaviesit@gmail.com>
2018-09-02 14:03:37 +0100

    created variables for project

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-// Target the button
-var button = document.querySelector('button');
+// Variables
 
+// Target the form, unordered list, button and input
+
+var form = document.querySelector('form');
+var todoList = document.querySelector('ul');
+var button = document.querySelector('button');
 // Also could have used
 // var button = document.querySelector('#clear');
 // var button = document.getElementById('clear');
+var input = document.getElementById('user-todo');
+
 
 button.addEventListener('click', function() {
     // alert("you have clicked the button"); Test
 })
 
-
-// Capture user input and stop whole page refreshing on submit
-
-var formField = document.querySelector('form');
-
-var input = document.getElementById('user-todo');
-
-formField.addEventListener('submit', function(e) {
+// Action, then call a function
+form.addEventListener('submit', function(e) {
     // keep page from refreshing
     e.preventDefault();
     // test
     alert(input.value)
 })
+
