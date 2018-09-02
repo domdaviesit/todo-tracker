c1b6f6325fcfbdea31b78bc47d5cf919169dc238
dom davies <domdaviesit@gmail.com>
2018-09-02 12:10:36 +0100

    targeted the button

---
+++ main.js
@@ -0,0 +1,10 @@
+// Target the button
+var button = document.querySelector('button');
+
+// Also could have used
+// var button = document.querySelector('#clear');
+// var button = document.getElementById('clear');
+
+button.addEventListener('click', function() {
+    // alert("you have clicked the button"); Test
+})
